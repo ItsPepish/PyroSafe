c9a9d196127a9bb97e8b37af1e8a3dd83ffd1d4c
Pepish <uriel.gonzalezr@hotmail.com>
2026-08-06 22:07:02 -0600

    Aside en Admin Dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,1 +1,17 @@
-<h1>Acceso al dashboard</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>PyroSafe Admin</title>
+</head>
+<body>
+    <div class="flex min-h-dvh bg-[#e5f1f5]/30">
+        <x-admin.aside/>
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
